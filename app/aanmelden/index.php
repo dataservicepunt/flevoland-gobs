d4019439a9cd70c74a34902ba035a0374cfe901c
Johan Groenen <johan.groenen+github@gmail.com>
2018-09-24 20:51:12 +0200

    style apps

--- app/aanmelden/index.php
+++ app/aanmelden/index.php
@@ -6,46 +6,66 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://www.dataservicepunt.nl/flevoland/css/style.css">
     <title>Aanmelden</title>
-
-    <style>
-      .container {
-        max-width: 32em !important;
-      }
-    </style>
   </head>
   <body>
+    <nav>
+      <div class="wrapper">
+        <a href="https://www.dataservicepunt.nl/flevoland/data/index.json">Dataportaal</a> |
+        <a href="https://www.dataservicepunt.nl/flevoland/apps/index.json">App store</a> |
+        <a href="mailto:flevoland@dataservicepunt.nl">flevoland@dataservicepunt.nl</a>
+      </div>
+    </nav>
 
-    <div class="container">
+    <header>
+      <div class="wrapper">
+        <img src="https://www.dataservicepunt.nl/flevoland/img/Logo.png" alt="Provincie Flevoland">
+      </div>
+    </header>
 
-      <form method="POST">
-        <fieldset>
-          <legend>Aanmelden</legend>
-
-          <p>Vul onderstaand formulier in om u aan te melden voor notificaties over de beschikbaarheid van bruggen en sluizen.</p>
-
-          <p>
-            <label for="telefoonnummer">Voer het telefoonnummer in waarop u notificaties wenst te ontvangen:</label><br>
-            <input type="text" id="telefoonnummer" name="telefoonnummer" placeholder="Telefoonnummer">
-          </p>
-          <p>Selecteer de bruggen en sluizen waarover u notificaties wenst te ontvangen:</p>
-          <p>
+    <main>
+      <div class="wrapper" style="padding-top: 8rem">
+        <div class="content" style="max-width: 40rem; background: white; padding: 1rem; margin: auto;">
+          <form method="POST">
+            <fieldset>
+              <legend>Aanmelden</legend>
+              <p>Vul onderstaand formulier in om u aan te melden voor notificaties over de beschikbaarheid van bruggen en sluizen.</p>
+              <p>
+                <label for="telefoonnummer">Voer het telefoonnummer in waarop u notificaties wenst te ontvangen:</label><br>
+                <input type="text" id="telefoonnummer" name="telefoonnummer" placeholder="Telefoonnummer">
+              </p>
+              <p>Selecteer de bruggen en sluizen waarover u notificaties wenst te ontvangen:</p>
+              <p>
 <?php foreach ($bruggen_en_sluizen["features"] as $feature) { ?>
-            <label>
-              <input type="checkbox" name="objecten[]" value="<?=$feature["properties"]["nummer"]?>">
-              <?=$feature["properties"]["naam"]?>
-            </label>
-            <br>
+                <label>
+                  <input type="checkbox" name="objecten[]" value="<?=$feature["properties"]["nummer"]?>">
+                  <?=$feature["properties"]["naam"]?>
+                </label>
+                <br>
 <? } ?>
-          </p>
+              </p>
+              <p><button>Selectie opslaan</button></p>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </main>
 
-          <p><button>Selectie opslaan</button></p>
-        </fieldset>
-      </form>
-    </div>
+    <footer>
+      <div class="wrapper">
+        <div style="display: flex">
+          <div style="width: 150px">
+            <img src="https://www.dataservicepunt.nl/flevoland/img/Logo-provincie-Flevoland.png?width=61&amp;height=83" alt="Logo Provincie Flevoland">
+          </div>
+          <div style="flex: auto">
+            <h2>Meer informatie en contact</h2>
+            <p><a href="mailto:flevoland@dataservicepunt.nl">flevoland@dataservicepunt.nl</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
--- app/versturen/index.php
+++ app/versturen/index.php
@@ -6,15 +6,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://www.dataservicepunt.nl/flevoland/css/style.css">
     <title>Notificatie versturen</title>
-
     <style>
-      .container {
-        max-width: 32em !important;
-      }
       textarea {
         width: 100%;
         min-height: 100px
@@ -22,34 +17,61 @@
     </style>
   </head>
   <body>
+    <nav>
+      <div class="wrapper">
+        <a href="https://www.dataservicepunt.nl/flevoland/data/index.json">Dataportaal</a> |
+        <a href="https://www.dataservicepunt.nl/flevoland/apps/index.json">App store</a> |
+        <a href="mailto:flevoland@dataservicepunt.nl">flevoland@dataservicepunt.nl</a>
+      </div>
+    </nav>
 
-    <div class="container">
+    <header>
+      <div class="wrapper">
+        <img src="https://www.dataservicepunt.nl/flevoland/img/Logo.png" alt="Provincie Flevoland">
+      </div>
+    </header>
 
-      <form method="POST">
-        <fieldset>
-          <legend>Notificatie versturen</legend>
-
-          <p>Vul onderstaand formulier in om een notificatie te versturen in het kader van de beschikbaarheid van bruggen en sluizen.</p>
-
-          <p>
-            <label for="notificatie">Vul de notificatie in die u wilt versturen:</label><br>
-            <textarea id="notificatie" name="notificatie" placeholder="Notificatie"></textarea>
-          </p>
-          <p>Selecteer de bruggen en sluizen waarop deze notificatie betrekking heeft:</p>
-          <p>
+    <main>
+      <div class="wrapper" style="padding-top: 8rem">
+        <div class="content" style="max-width: 40rem; background: white; padding: 1rem; margin: auto;">
+          <form method="POST">
+            <fieldset>
+              <legend>Notificatie versturen</legend>
+              <p>Vul onderstaand formulier in om een notificatie te versturen in het kader van de beschikbaarheid van bruggen en sluizen.</p>
+              <p>
+                <label for="notificatie">Vul de notificatie in die u wilt versturen:</label><br>
+                <textarea id="notificatie" name="notificatie" placeholder="Notificatie"></textarea>
+              </p>
+              <p>Selecteer de bruggen en sluizen waarop deze notificatie betrekking heeft:</p>
+              <p>
 <?php foreach ($bruggen_en_sluizen["features"] as $feature) { ?>
-            <label>
-              <input type="checkbox" name="objecten[]" value="<?=$feature["properties"]["nummer"]?>">
-              <?=$feature["properties"]["naam"]?>
-            </label>
-            <br>
+                <label>
+                  <input type="checkbox" name="objecten[]" value="<?=$feature["properties"]["nummer"]?>">
+                  <?=$feature["properties"]["naam"]?>
+                </label>
+                <br>
 <? } ?>
-          </p>
+              </p>
+              <p><button>Notificatie versturen</button></p>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </main>
 
-          <p><button>Notificatie versturen</button></p>
-        </fieldset>
-      </form>
-    </div>
+    <footer>
+      <div class="wrapper">
+        <div style="display: flex">
+          <div style="width: 150px">
+            <img src="https://www.dataservicepunt.nl/flevoland/img/Logo-provincie-Flevoland.png?width=61&amp;height=83" alt="Logo Provincie Flevoland">
+          </div>
+          <div style="flex: auto">
+            <h2>Meer informatie en contact</h2>
+            <p><a href="mailto:flevoland@dataservicepunt.nl">flevoland@dataservicepunt.nl</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
