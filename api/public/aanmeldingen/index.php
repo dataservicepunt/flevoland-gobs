c64c5f73785435deb56fe9e937428f0dac84a37c
Johan Groenen <johan@tiltshift.nl>
2018-09-29 11:01:14 +0200

    live fixes

--- api/public/aanmeldingen/index.php
+++ api/public/aanmeldingen/index.php
@@ -12,7 +12,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
   case "POST":
     if ($input = $_POST) {
       // void
-    } elseif ($input = json_decode(file_get_contents("php://input"))) {
+    } elseif ($input = json_decode(file_get_contents("php://input"), true)) {
       // void
     } else {
       header("HTTP/1.1 400 Bad Request");
