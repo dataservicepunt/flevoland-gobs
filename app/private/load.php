8c38f3d6932d807066bafd35f0ac269d95ea9f60
Johan Groenen <johan.groenen+github@gmail.com>
2018-10-24 14:12:35 +0200

    order

--- app/private/load.php
+++ app/private/load.php
@@ -7,9 +7,15 @@ $cdnRoot = "https://www.dataservicepunt.nl/flevoland";
 
 $provinciale_wegen_url = "{$cdnRoot}/data/provinciale_wegen/provinciale_wegen.geojson";
 $provinciale_wegen = json_decode(file_get_contents($provinciale_wegen_url), true);
+usort($provinciale_wegen["features"], function ($a, $b) {
+  return $a["properties"]["NAAM"] > $b["properties"]["NAAM"];
+});
 
 $bruggen_en_sluizen_url = "{$cdnRoot}/data/bruggen_en_sluizen/bruggen_en_sluizen.geojson";
 $bruggen_en_sluizen = json_decode(file_get_contents($bruggen_en_sluizen_url), true);
+usort($bruggen_en_sluizen["features"], function ($a, $b) {
+  return $a["properties"]["naam"] > $b["properties"]["naam"];
+});
 
 $navHtml = file_get_contents("{$cdnRoot}/partial_nav.html");
 $footerHtml = file_get_contents("{$cdnRoot}/partial_footer.html");
