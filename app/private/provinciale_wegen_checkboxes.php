2b5cda2531b296953b98d5394e641a855b3c5768
Johan Groenen <johan.groenen+github@gmail.com>
2019-08-18 14:58:47 +0200

    split wegen in NOP and Flevopolders

--- app/private/provinciale_wegen_checkboxes.php
+++ app/private/provinciale_wegen_checkboxes.php
@@ -6,8 +6,21 @@
   usort($wegen, function ($a, $b) {
     return $b["naam"] < $a["naam"];
   });
-  foreach ($wegen as $weg) {
-    if (!empty($weg["communicatie_naam"])) {
+  $wegen = array_filter($wegen, function ($weg) {
+    return !empty($weg["communicatie_naam"]);
+  });
+
+  $wegen_NOP = array_filter($wegen, function ($weg) {
+    return $weg["regio"] === "NOP";
+  });
+
+  $wegen_F = array_filter($wegen, function ($weg) {
+    return $weg["regio"] === "OF" || $weg["regio"] === "ZF";
+  });
+
+  echo "<h2 style='margin: 20px 0'>Flevopolders</h2>";
+
+  foreach ($wegen_F as $weg) {
 ?>
     <label>
       <input type="checkbox" name="objecten[]" value="<?php echo $weg["id"]; ?>">
@@ -20,5 +33,21 @@
     </label>
     <br>
 <?php
-    } // if
+  } // foreach
+
+  echo "<h2 style='margin: 20px 0'>Noordoostpolder en Urk</h2>";
+
+  foreach ($wegen_NOP as $weg) {
+?>
+    <label>
+      <input type="checkbox" name="objecten[]" value="<?php echo $weg["id"]; ?>">
+      <?php
+        echo $weg["communicatie_naam"];
+        if (!empty($includeStats) && !empty($stats["objecten"][$weg["id"]])) {
+          echo " ({$stats["objecten"][$weg["id"]]})";
+        }
+      ?>
+    </label>
+    <br>
+<?php
   } // foreach
